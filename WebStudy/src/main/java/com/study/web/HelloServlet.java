5965d7335cf69eff3fa67b87d7fe3193b4e9bb8e
Auden <sasameyuki03@naver.com>
2022-10-26 21:38:39 +0900

    2022-10-26 Hello Servlet! 서버 공부 첫 시작. 필기는 에버노트에.

---
+++ WebStudy/src/main/java/com/study/web/HelloServlet.java
@@ -0,0 +1,20 @@
+package com.study.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/hi") //괄호 안에는 url을 넣는다. 꼭 파일과 관련된 내용을 넣는게 아니다!!
+public class HelloServlet extends HttpServlet{
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) 
+			throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("Hello Servlet!!");
+	}
+}
